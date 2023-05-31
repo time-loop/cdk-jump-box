ea6199ff11f690554a11f65efba34e0f6296af50
Ryan Yee <ryee88@gmail.com>
2023-05-31 16:04:22 -0700

    feat(core-services): CDK | Add override for desired jumpbox capacity [RDMP-4640]

--- src/index.ts
+++ src/index.ts
@@ -53,6 +53,9 @@ export interface JumpBoxProps {
    * @default - default subnet selection
    */
   readonly vpcSubnets?: aws_ec2.SubnetSelection;
+  readonly desiredCapacity?: number;
+  readonly minCapacity?: number;
+  readonly maxCapacity?: number;
 }
 
 /**
@@ -116,8 +119,9 @@ export class JumpBox extends Construct {
       instanceType,
       keyName: this.keyPair.keyPairName,
       machineImage,
-      minCapacity: 0,
-      maxCapacity: 1,
+      desiredCapacity: props.desiredCapacity,
+      minCapacity: props.minCapacity ?? 0,
+      maxCapacity: props.maxCapacity ?? 1,
       role: this.role,
       securityGroup: this.securityGroup,
       signals: aws_autoscaling.Signals.waitForMinCapacity({ timeout: Duration.minutes(45) }),
